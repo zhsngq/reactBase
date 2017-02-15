fe8a468a432b12333b7bb3bdb805db1a781c4dfc
zhusongqi <815723901@qq.com>
2017-02-15 17:36:51 +0800

    添加组件视例

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import MyAwesomeReactComponent from './MyAwesomeReactComponent';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import Hello from './Hello';
+
 injectTapEventPlugin();
 
 class App extends Component {
-  render() {
-    return (
-        <MuiThemeProvider>
-            <MyAwesomeReactComponent />
-        </MuiThemeProvider>
-    );
-  }
+    render() {
+        return (
+            <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+                <Hello />
+            </MuiThemeProvider>
+        );
+    }
 }
 
 export default App;
